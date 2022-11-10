b37f6f2434a08834c3701f81f5c52823c76367de
Lance Yoder <lyoder@princeton.edu>
2022-11-10 15:22:50 -0500

    random moves, works for xboard

--- engine.py
+++ engine.py
@@ -7,6 +7,8 @@
 
 import chess
 import numpy as np
+import time
+import argparse
 
 import sys, codecs
 
@@ -18,31 +20,30 @@ if sys.stdout.encoding is None or sys.stdout.encoding == 'ANSI_X3.4-1968':
         sys.stdout = utf8_writer(sys.stdout.buffer, errors='replace')
 
 def charToUni(c):
-    match c:
-        case 'P':
-            return u'\N{WHITE CHESS PAWN}'
-        case 'R':
-            return u'\N{WHITE CHESS ROOK}'
-        case 'N':
-            return u'\N{WHITE CHESS KNIGHT}'
-        case 'B':
-            return u'\N{WHITE CHESS BISHOP}'
-        case 'Q':
-            return u'\N{WHITE CHESS QUEEN}'
-        case 'K':
-            return u'\N{WHITE CHESS KING}'
-        case 'p':
-            return u'\N{BLACK CHESS PAWN}'
-        case 'r':
-            return u'\N{BLACK CHESS ROOK}'
-        case 'n':
-            return u'\N{BLACK CHESS KNIGHT}'
-        case 'b':
-            return u'\N{BLACK CHESS BISHOP}'
-        case 'q':
-            return u'\N{BLACK CHESS QUEEN}'
-        case 'k':
-            return u'\N{BLACK CHESS KING}'
+    if c == 'P':
+        return u'\N{WHITE CHESS PAWN}'
+    if c == 'R':
+        return u'\N{WHITE CHESS ROOK}'
+    if c == 'N':
+        return u'\N{WHITE CHESS KNIGHT}'
+    if c == 'B':
+        return u'\N{WHITE CHESS BISHOP}'
+    if c == 'Q':
+        return u'\N{WHITE CHESS QUEEN}'
+    if c == 'K':
+        return u'\N{WHITE CHESS KING}'
+    if c == 'p':
+        return u'\N{BLACK CHESS PAWN}'
+    if c == 'r':
+        return u'\N{BLACK CHESS ROOK}'
+    if c == 'n':
+        return u'\N{BLACK CHESS KNIGHT}'
+    if c == 'b':
+        return u'\N{BLACK CHESS BISHOP}'
+    if c == 'q':
+        return u'\N{BLACK CHESS QUEEN}'
+    if c == 'k':
+        return u'\N{BLACK CHESS KING}'
 
 def printfen(fen):
     rows = fen.split('/')
@@ -60,57 +61,145 @@ def printfen(fen):
     print('  a b c d e f g h')
 
 
-board = chess.Board()
 
-boardfen = board.fen().split(' ', 1)[0]
+def mainTerminal():
 
-printfen(boardfen)
-print("------------------")
+    my_file = open("my_file.txt", "w")
+    my_file.write(f'timeeex: {time.time()} \n')
+    my_file.write("wrote input?")
+    sys.stdout.write("in mainTerminal\n")
+    sys.stdout.flush()
 
-while not board.is_game_over():
-    try:
-        INPUT = input("Move:\n")
-        move = chess.Move.from_uci(INPUT)
-        while move not in board.legal_moves:
-            INPUT = input("Not a legal move. Try again:\n")
-            print("input: ", INPUT)
+    board = chess.Board()
+
+    boardfen = board.fen().split(' ', 1)[0]
+
+    printfen(boardfen)
+    print("------------------")
+
+    while not board.is_game_over():
+        try:
+            INPUT = input("Move:\n")
+
+            my_file.write("INput from xboard: ")
+            my_file.write(INPUT)
+            sys.stdout.write("input FFrom xboadD\n")
+            sys.stdout.write(INPUT)
+            sys.stdout.flush()
+            print("Input from xboard: ", INPUT)
+
             move = chess.Move.from_uci(INPUT)
+            while move not in board.legal_moves:
+                INPUT = input("Not a legal move. Try again:\n")
+                print("input: ", INPUT)
+                move = chess.Move.from_uci(INPUT)
 
-        board.push(move)
+            board.push(move)
 
-        if board.is_game_over():
-            continue
+            if board.is_game_over():
+                continue
 
-        moves = board.generate_legal_moves()
-        numlegal = board.legal_moves.count()
-        num = np.random.randint(numlegal)
-        i = 0
-        for move in moves:
-            if i == num:
-                damove = move
-                break
-            i += 1
-        board.push(damove)
-        boardfen = board.fen().split(' ', 1)[0]
+            moves = board.generate_legal_moves()
+            numlegal = board.legal_moves.count()
+            num = np.random.randint(numlegal)
+            i = 0
+            for move in moves:
+                if i == num:
+                    damove = move
+                    break
+                i += 1
+            board.push(damove)
+            boardfen = board.fen().split(' ', 1)[0]
 
-        printfen(boardfen)
-        print("------------------")
-    except Exception as ex:
-        print("Oops: ", ex)
+            printfen(boardfen)
+            #print("------------------")
+        except Exception as ex:
+            print("oopss, ", ex)
+            my_file.write("exception")
 
-boardfen = board.fen().split(' ', 1)[0]
-printfen(boardfen)
-if board.outcome().winner:
-    print("White wins")
-else:
-    print("Black wins")
+    boardfen = board.fen().split(' ', 1)[0]
+    printfen(boardfen)
+    if board.outcome().winner:
+        print("White wins")
+        my_file.write("White wins")
+    else:
+        print("Black wins")
+        my_file.write("Black wins")
+
+    my_file.close()
+
+def setup():
+    board = chess.Board()
+
+    boardfen = board.fen().split(' ', 1)[0]
+
+    return board, boardfen
+
+def main():
+    my_file = open("my_file.txt", "w")
+    my_file.write(f'timeeex: {time.time()} \n')
+    my_file.write("wrote input?\n")
+
+    board, boardfen = setup()
+
+    sys.stdout.write("feature done=0" + "\n")
+    sys.stdout.write("feature reuse=0" + "\n")
+    sys.stdout.write("feature done=1" + "\n")
+    sys.stdout.flush()
+
+    try:
+        processInput(my_file, board, boardfen)
+    finally:
+        my_file.close()
+    return
+
+
+def processInput(my_file, board, boardfen):
+    while True:
+        INPUT = "NOPE"
+        try:
+            INPUT = input()
+        except KeyboardInterrupt as ki:
+            sys.stdout.write(repr(ki))
+            sys.stdout.write(INPUT)
+            sys.stdout.flush()
+
+        if INPUT == "":
+            break
+        inArray = INPUT.rsplit()
+        try:
+            move = chess.Move.from_uci(INPUT)
+            board.push(move)
+
+            moves = board.generate_legal_moves()
+            numlegal = board.legal_moves.count()
+            num = np.random.randint(numlegal)
+            i = 0
+            for move in moves:
+                if i == num:
+                    damove = move
+                    break
+                i += 1
+            board.push(damove)
+            #time.sleep(1)
+            sys.stdout.write("move " + damove.uci() + "\n")
+            sys.stdout.flush()
+        except Exception as ex:
+            sys.stdout.write(repr(ex) + "\n")
+            sys.stdout.write(INPUT + "\n")
+            sys.stdout.flush()
+    return
+
+def eval(board):
 
 
 
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'terminal':
+            mainTerminal()
+        else:
+            print("Usage: python engine.py terminal")
+    else:
+        main()
